33bef7ae71d209663d4a3cfc1b3811235f630eb5
Ryc O'Chet <rycochet@rycochet.com>
2015-01-28 22:25:34 +0000

    Improve warning for the injected script since we link to this page

--- Tabs to the Front/keypress.js
+++ Tabs to the Front/keypress.js
@@ -1,6 +1,14 @@
 /*
- * Listen for the Ctrl key (Command on Mac) and remember it's pressed state
+ * Listen for the Ctrl key (Command on Mac) and remember it's pressed state.
+ * 
+ * IMPORTANT: This requires additional permission to run. The permission can
+ * also be used to give full access to *everything*, so if you're not sure
+ * what's going on then DO NOT ALLOW IT.
+ * 
+ * This is the source code for "Tabs to the Front" -
+ * https://chrome.google.com/webstore/detail/tabs-to-the-front/hiembaoomcehoiehhdldabfgnmphappc
  */
+
 function sendCtrl(event) {
 	chrome.storage.local.set({
 		ctrl: event.ctrlKey
