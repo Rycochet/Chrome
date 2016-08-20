ee99ea106f11a24da2998019c4610dc22e5a5a7c
Ryc O'Chet <rycochet@rycochet.com>
2016-08-20 17:00:25 +0100

    Fix event links for long events, don't leak calendar icon grabbing to google

--- FetLife+/event.js
+++ FetLife+/event.js
@@ -6,6 +6,8 @@
 (function($, chrome, sync) {
 	"use strict";
 
+	var maxDescriptionLength = 4000;
+
 	$.fn.ignore = function(sel) {
 		return this.clone().find(sel || ">*").remove().end();
 	};
@@ -31,18 +33,18 @@
 				}).join(", "),
 				startDate = new Date($("meta[itemprop=startDate]").attr("content")),
 				endDate = new Date($("meta[itemprop=endDate]").attr("content")),
-				googleIconLink = "https://calendar.google.com/googlecalendar/images/favicon_v2014_" + startDate.getDate() + ".ico",
+				googleIconLink = chrome.extension.getURL("google_calendar.png"),
 				addToCalendarLink = "https://www.google.com/calendar/render?action=TEMPLATE&" +
 				"text=" + encodeURIComponent(title) +
 				"&dates=" + formatTime(startDate) + "/" + formatTime(endDate) +
 				"&location=" + encodeURIComponent(location) +
 				"&sprop=" + encodeURIComponent(eventLink) +
 				"&sprop=name:fetlife" +
-				"&details=" + encodeURIComponent(desc) +
+				"&details=" + encodeURIComponent(eventLink) + (desc ? "%0A%0A" + encodeURIComponent(desc.length > maxDescriptionLength ? desc.substr(0, maxDescriptionLength) + "..." : desc) : "") +
 				"&sf=true&output=xml";
 
-		$("<a target=\"_blank\" href=\"" + addToCalendarLink + "\" class=\"xs q tdn\" style=\"margin-left:10px\">"
-				+ "<img src=\"" + googleIconLink + "\" title=\"" + chrome.i18n.getMessage("addToGoogleCalendar") + "\" alt=\"+gCal\" style=\"height:1.5em;background:none;float:left;padding:0 0.4em 0 0;\">"
+		$("<a target=\"_blank\" href=\"" + addToCalendarLink + "\" class=\"xs q tdn\" style=\"float:right;background:none;\">"
+				+ "<img src=\"" + googleIconLink + "\" title=\"" + chrome.i18n.getMessage("addToGoogleCalendar") + "\" alt=\"+gCal\" style=\"height:25px;padding:0;\">"
 				+ "</a>")
 				.appendTo("table.list.mbn .db>.l");
 	});
